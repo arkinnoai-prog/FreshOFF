3a8e40234cdcc7e5c2eb669c13a9341e995fb798
donex <donex6fience@gmail.com>
2025-10-08 11:04:35 +0530

    [prod/fix : 404 error while navagation or relaod fix due to lack of verce.json fixed and added. realtive issue fixed ]

--- src/assets/outfit.ts
+++ src/assets/outfit.ts
@@ -1,37 +1,54 @@
+import bag222 from "../assets/bags/bag222.png";
+import bag111 from "../assets/bags/bag111.png";
+import bag333 from "../assets/bags/bag333.png";
+import c4 from "../assets/bags/categories/c4.png";
+import blue1 from "../assets/bags/featured-products/blue1.png";
+import t2 from "../assets/outfits/bags/tote-bag/t2.png";
+import orange2 from "../assets/bags/featured-products/orange2.png";
+import rose2 from "../assets/bags/featured-products/rose2.png";
+import white1 from "../assets/bags/featured-products/white1.png";
+import bag555 from "../assets/bags/bag555.png";
+import t3 from "../assets/outfits/bags/tote-bag/t3.png";
+import c1 from "../assets/bags/categories/c1.png";
+import bag444 from "../assets/bags/bag444.png";
+import c2 from "../assets/bags/categories/c2.png";
+import black1 from "../assets/bags/featured-products/black1.png";
+import c3 from "../assets/bags/categories/c3.png";
+
 export const sampleOutfits = [
   // Box Clutch
   {
     id: "bag-1",
     name: "Midnight Black Box Clutch",
-    url: "src/assets/bags/bag222.png",
+    url: bag222,
     category: "bag",
     subCategory: "box-clutch",
   },
   {
     id: "bag-2",
     name: "Golden Glow Evening Clutch",
-    url: "src/assets/bags/bag111.png",
+    url: bag111,
     category: "bag",
     subCategory: "box-clutch",
   },
   {
     id: "bag-3",
     name: "Mysterious Sapphire Clutch",
-    url: "src/assets/bags/bag333.png",
+    url: bag333,
     category: "bag",
     subCategory: "box-clutch",
   },
   {
     id: "bag-4",
     name: "White Crystal Box Clutch",
-    url: "src/assets/bags/categories/c4.png",
+    url: c4,
     category: "bag",
     subCategory: "box-clutch",
   },
   {
     id: "bag-5",
     name: "Royal Blue Velvet Clutch",
-    url: "src/assets/bags/featured-products/blue1.png",
+    url: blue1,
     category: "bag",
     subCategory: "box-clutch",
   },
@@ -40,77 +57,77 @@ export const sampleOutfits = [
   {
     id: "bag-6",
     name: "Classic Canvas Tote",
-    url: "src/assets/outfits/bags/tote-bag/t2.png",
+    url: t2,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-7",
     name: "Sunset Orange Satchel",
-    url: "src/assets/bags/featured-products/orange2.png",
+    url: orange2,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-8",
     name: "Rose Blush Satchel",
-    url: "src/assets/bags/featured-products/rose2.png",
+    url: rose2,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-9",
     name: "Ivory White Satchel",
-    url: "src/assets/bags/featured-products/white1.png",
+    url: white1,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-10",
     name: "Emerald Green Satchel",
-    url: "src/assets/bags/bag555.png",
+    url: bag555,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-11",
     name: "Urban Gray Tote",
-    url: "src/assets/outfits/bags/tote-bag/t3.png",
+    url: t3,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-12",
     name: "Tropical Beach Bag",
-    url: "src/assets/bags/categories/c1.png",
+    url: c1,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-13",
     name: "Shiny Pearl Handbag",
-    url: "src/assets/bags/bag444.png",
+    url: bag444,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-14",
     name: "Soft Beige Tote",
-    url: "src/assets/bags/categories/c2.png",
+    url: c2,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-15",
     name: "Elegant Black Leather Satchel",
-    url: "src/assets/bags/featured-products/black1.png",
+    url: black1,
     category: "bag",
     subCategory: "Satchel",
   },
   {
     id: "bag-16",
     name: "Golden Haze Satchel",
-    url: "src/assets/bags/categories/c3.png",
+    url: c3,
     category: "bag",
     subCategory: "Satchel",
   },
